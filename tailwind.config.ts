8090621c4752699edb24698522089db0bd32aea2
DineshDavuluri <130375524+DineshDavuluri@users.noreply.github.com>
2025-05-05 18:18:37 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,17 @@
 
 import type { Config } from "tailwindcss";
-
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
-	],
-	prefix: "",
-	theme: {
+		"./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}",	"./app/**/*.{ts,tsx}",	"./src/**/*.{ts,tsx}",],
+	prefix:"",
+	theme:{
 		container: {
 			center: true,
 			padding: '2rem',
 			screens: {
 				'2xl': '1400px'
-			}
-		},
+			}},
 		extend: {
 			colors: {
 				border: 'hsl(var(--border))',
