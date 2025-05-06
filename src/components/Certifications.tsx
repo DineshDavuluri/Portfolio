0a283f47f3cc2b0d23fd4415466b07fd3c112d67
DineshDavuluri <130375524+DineshDavuluri@users.noreply.github.com>
2025-05-06 10:19:11 +0530

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -23,7 +23,7 @@ const certifications = [
   },{
     title: "Udemy Full Stack Certificate",
     description: "Developed 5 capstone projects aaong with a Full Stack Project(Alumni Social Media Website)",
-    icon: "globe",
+    icon: "trophy",
     image: ""
   }
 ];
@@ -83,6 +83,12 @@ const Certifications = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
           </svg>
         );
+        case 'trophy':
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 21h8M12 17v4m4-18v5a4 4 0 01-8 0V3m-2 4h12a2 2 0 002-2V3H4v2a2 2 0 002 2z" />
+    </svg>
+  );
       case 'globe':
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
